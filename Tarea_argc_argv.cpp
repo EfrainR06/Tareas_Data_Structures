8c292c446524f0c361fbd5de4631b1ba8507907f
Pachaconjettt <efrain.retana.segura@est.una.ac.cr>
2025-07-23 22:42:42 -0600

    Tercera tarea terminada

---
+++ Tarea_argc_argv.cpp
@@ -0,0 +1,19 @@
+//Efrain Retana Segura UNA
+#include <iostream>
+#include <string>
+using namespace std;
+// int main(int argc, char* argv[]) {
+//     cout << "Cantidad: " << argc << endl;
+//     for (int i = 0; i < argc; i++) {
+//         cout << "Arg["<< i <<"]: " << argv[i] << endl;
+//     }
+// }
+// int main(int argc, char* argv[]) {
+//     if (argc != 3) {
+//         cout << "Uso: ./suma num1 num2";
+//         return 1;
+//     }
+//     int a = atoi(argv[1]);
+//     int b = atoi(argv[2]);
+//     cout << "Suma: " << a + b;
+// }
